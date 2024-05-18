6d18bf10ceb8b65ae7869e02735ed4b961d0e9a2
ttaylor-st <ttaylor-st@murena.io>
2024-05-18 14:49:02 +0100

    feat: parse markdown in post bodies, add styles for code blocks

--- src/components/post/PostBody.vue
+++ src/components/post/PostBody.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { markdownToHtml } from '@/lib/markdown'
 
 const props = defineProps({
   post: {
     type: Object,
     required: true
+  },
+  fullBody: {
+    type: Boolean,
+    default: false
   }
 })
 
 const post = ref(props.post)
+const body = markdownToHtml(post.value.body)
 </script>
 
 <template>
   <div class="post-body">
     <h2>{{ post.title }}</h2>
-    <p>{{ post.body }}</p>
+    <p class="content" v-html="body" :style="{ maxHeight: full ? 'none' : 'calc(1.5rem * 3)' }"></p>
   </div>
 </template>
 
@@ -26,11 +32,17 @@ const post = ref(props.post)
     font-size: 1.5rem;
   }
 
-  p {
-    white-space: pre-wrap;
-    max-height: calc(1.5rem * 3);
+  .content {
     overflow: hidden;
     position: relative;
+    word-wrap: break-word;
   }
 }
+
+img {
+  width: 100%;
+  height: auto;
+  max-height: 10em;
+}
+
 </style>
