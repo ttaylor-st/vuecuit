46fae10731f39c0407f1de097f507fd64d37f3f2
ttaylor-st <ttaylor-st@murena.io>
2024-05-21 11:16:44 +0100

    refactor: update getProfilePicture function to handle optional target

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import type { Community, User } from '@/types/discuitTypes'
 import { useUrlStore } from '@/stores/urlStore'
 
+export function randomString(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return result
+}
+
 export function timeSince(postCreatedAt: Date): string {
   const postTime = new Date(postCreatedAt).getTime()
   const currentTime = new Date().getTime()
@@ -24,8 +33,15 @@ export function timeSince(postCreatedAt: Date): string {
   return 'Just now'
 }
 
-export function getProfilePicture(target: User | Community): string {
+export function getProfilePicture(target?: User | Community): string {
   const urlStore = useUrlStore()
-  if (target.proPic) return `${urlStore.url}/${target.proPic.url}`
-  return `https://api.dicebear.com/8.x/identicon/svg?seed=${target.id}`
+  const baseUrl = urlStore.url
+  const defaultPicUrl = 'https://api.dicebear.com/8.x/identicon/svg?seed='
+
+  if (target) {
+    if (target.proPic) return `${baseUrl}/${target.proPic.url}`
+    return `${defaultPicUrl}${target.id}`
+  }
+
+  return `${defaultPicUrl}${randomString(10)}`
 }
