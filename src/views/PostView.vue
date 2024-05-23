03886bd5d1e00e8c6965f2650c7013ca75f07a81
ttaylor-st <ttaylor-st@murena.io>
2024-05-23 23:50:48 +0100

    feat: add footer to PostView

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -5,6 +5,7 @@ import { useUrlStore } from '@/stores/urlStore'
 import PostHeader from '@/components/post/PostHeader.vue'
 import PostBody from '@/components/post/PostBody.vue'
 import { ref } from 'vue'
+import PostFooter from '@/components/post/PostFooter.vue'
 
 const userStore = useUserStore()
 const urlStore = useUrlStore()
@@ -15,21 +16,44 @@ const post = await userStore
   .makeRequest(`${urlStore.apiUrl}/posts/${publicId.value}`, 'GET')
   .then((res) => res.json())
 
-// TODO: Fetch comments
-
+const comments = await userStore
+  .makeRequest(`${urlStore.apiUrl}/posts/${publicId.value}/comments`, 'GET')
+  .then((res) => res.json())
 </script>
 
 <template>
 
-  <main>
+
     <section class="post">
       <PostHeader :post="post" />
       <PostBody :post="post" :full-body="true" />
+      <PostFooter :post="post" />
     </section>
-  </main>
+
+    <section class="comments">
+      <h2>Comments</h2>
+    </section>
+
 
 </template>
 
 <style scoped>
 
+.post {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  background-color: hsl(var(--primary-200) / 0.25);
+  border: 1px solid transparent;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  transition: all 0.3s ease;
+}
+
+.comments {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
