d613b7cd9bfb1f47677d039cd003405d617577c1
Taylor St. <ttaylor-st@murena.io>
2024-06-05 23:36:34 +0100

    refactor: move Post css to _post.scss

--- src/components/post/PostFooter.vue
+++ src/components/post/PostFooter.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useUserStore } from '@/stores/userStore'
 import { useUrlStore } from '@/stores/urlStore'
+import { useUserStore } from '@/stores/userStore'
+import { ref } from 'vue'
 
-import ArrowUpBold from 'vue-material-design-icons/ArrowUpBold.vue'
-import ArrowDownBold from 'vue-material-design-icons/ArrowDownBold.vue'
 import type { Post } from '@/types/discuitTypes'
+import ArrowDownBold from 'vue-material-design-icons/ArrowDownBold.vue'
+import ArrowUpBold from 'vue-material-design-icons/ArrowUpBold.vue'
 
 const userStore = useUserStore()
 const urlStore = useUrlStore()
@@ -50,59 +50,3 @@ async function vote(upvote: boolean) {
   </div>
 
 </template>
-
-<style scoped>
-
-.post-footer {
-  display: flex;
-  gap: 0.5rem;
-  align-items: stretch;
-}
-
-.footer-pill {
-  user-select: none;
-  padding: 0 1rem;
-  background-color: hsla(var(--primary-200) / 0.5);
-  border: 1px solid hsla(var(--primary-600) / 0.5);
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  border-radius: 5000px;
-  transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1);
-
-  &:hover {
-    background-color: hsla(var(--primary-200) / 0.75);
-  }
-
-  &:active {
-    background-color: hsla(var(--primary-300) / 0.75);
-    border: 1px solid hsla(var(--primary-600) / 1);
-  }
-
-}
-
-.votes {
-  padding: 0;
-
-  button {
-    background-color: hsla(var(--primary-600) / 0.5);
-    border: none;
-    border-radius: 50%;
-    padding: 0.5rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1);
-
-    span {
-      color: hsla(var(--primary-950) / 1);
-    }
-
-    &:hover {
-      background-color: hsla(var(--primary-600) / 0.75);
-    }
-  }
-}
-
-</style>
