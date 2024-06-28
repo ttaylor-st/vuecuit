568682e3f57bdd468d190f667824bffdf855b0f9
Taylor <ttaylor-st@murena.io>
2024-06-29 00:49:45 +0100

    refactor: take in a Comment instead of comment ID
    temp added css

--- src/components/comments/EmbeddedComment.vue
+++ src/components/comments/EmbeddedComment.vue
@@ -1,58 +1,92 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { useUserStore } from "@/stores/userStore";
 import { useUrlStore } from "@/stores/urlStore";
 import { useRouter } from "vue-router";
 
 import type { Comment } from "@/types/discuitTypes";
+import {timeSince} from "@/lib/utils";
+import {markdownToHtml} from "@/lib/markdown";
 
 const userStore = useUserStore();
 const urlStore = useUrlStore();
 const router = useRouter();
 
-const loading = ref(true);
 const props = defineProps({
-	postPublicId: {
-		type: String,
-		required: true,
-	},
-	publicId: {
-		type: String,
-		required: true,
-	},
+	comment: {
+    type: Object as () => Comment,
+    required: true,
+  },
 });
 
-const publicId = ref(props.publicId);
-const comment = ref<Comment>();
+const timeElapsedHuman = timeSince(props.comment.createdAt);
+const humanReadableDate = new Date(props.comment.createdAt).toLocaleString();
 
-onMounted(async () => {
-	comment.value = await userStore
-		.makeRequest(
-			`${urlStore.apiUrl}/posts/${props.postPublicId}/comments/${publicId.value}`,
-			"GET",
-		)
-		.then((res) => res.json())
-		.catch((err) => console.error(err));
-
-	loading.value = false;
+const body = computed(() => {
+  return markdownToHtml(props.comment.body);
 });
+
 </script>
 
 <template>
 
-  <div class="embedded-comment" v-if="!loading">
+  <div class="embedded-comment">
     <div class="comment-header">
       <div class="comment-user">
-        <img class="comment-user-icon" :src="comment.user.profilePicture" alt="User profile picture">
-        <span class="comment-user-name">{{ comment.user.username }}</span>
+        <img class="comment-user-icon" :src="comment.author.proPic?.url" alt="User profile picture">
+        <span class="comment-user-name">{{ comment.author.username }}</span>
       </div>
-      <span class="comment-date">{{ new Date(comment.createdAt).toLocaleDateString() }}</span>
+      <span class="comment-date" :title="humanReadableDate">{{ timeElapsedHuman }}</span>
     </div>
-    <div class="comment-body" v-html="comment.body"></div>
+    <div class="comment-body" v-html="body"></div>
   </div>
 
 </template>
 
 <style scoped>
+
+.embedded-comment {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  background-color: hsl(var(--primary-200) / 0.25);
+  border: 1px solid transparent;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  transition: all 0.3s ease;
+}
+
+.comment-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.comment-user {
+  display: flex;
+  align-items: center;
+}
+
+.comment-user-icon {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 0.5rem;
+}
+
+.comment-user-name {
+  font-weight: bold;
+}
+
+.comment-date {
+  font-size: 0.8rem;
+}
+
+.comment-body {
+  margin-top: 0.5rem;
+}
+
+
 
 </style>
