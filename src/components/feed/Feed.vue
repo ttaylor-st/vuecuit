1fa9bb961ec1ce9750259af88c223ea15fb7f902
Taylor <ttaylor-st@murena.io>
2024-06-29 02:00:11 +0100

    feat: add `Feed` and `FeedItem` components
    `Feed` handles fetching posts and comments depending on the provided feed type ('user', 'community', 'home') and displays them. FeedItem handles the rendering of individual posts or comments within the feed.

---
+++ src/components/feed/Feed.vue
@@ -0,0 +1,130 @@
+<script setup lang="ts">
+import type { Comment, Post } from '@/types/discuitTypes'
+
+import { defineProps, ref, onUnmounted, onMounted } from 'vue'
+import {useUserStore} from "@/stores/userStore";
+import {useUrlStore} from "@/stores/urlStore";
+
+import PostComponent from '@/components/post/PostComponent.vue'
+import EmbeddedComment from '@/components/comments/EmbeddedComment.vue'
+import FeedItem from '@/components/feed/FeedItem.vue'
+
+const props = defineProps({
+  username: {
+    type: String,
+    required: false
+  },
+  communityId: {
+    type: String,
+    required: false
+  },
+  // "user", "community", "home"
+  type: {
+    type: String,
+    required: true,
+    default: 'home'
+  },
+  sort: {
+    type: String,
+    required: false,
+    default: 'hot'
+  },
+  userType: {
+    type: String,
+    required: false,
+    default: 'overview'
+  }
+})
+const next = ref<string | null>(null)
+const isFetching = ref(false)
+
+const userStore = useUserStore()
+const urlStore = useUrlStore()
+
+type FeedItem = {
+  type: string,
+  item: Post | Comment
+}
+
+type Feed = {
+  items: FeedItem[]
+  next: string | null
+}
+
+type Posts = {
+  posts: Post[]
+  next: string | null
+}
+
+const userFeed = ref<Feed[]>([])
+const posts = ref<Post[]>([])
+
+async function fetchPosts() {
+  if (isFetching.value) return
+  isFetching.value = true
+  console.log('fetching posts')
+
+  if (props.type === 'user') {
+    const filter = props.userType === 'overview' ? '' : props.userType
+    const url = `${urlStore.apiUrl}/users/${props.username}/feed?filter=${filter}&next=${next.value || ''}`
+    const response = await userStore.makeRequest(url, 'GET')
+    const data: Feed = await response.json() || {items: [], next: null}
+
+    for (const feed of data.items) userFeed.value.push(feed)
+    next.value = data.next
+
+  } else if (props.type === 'community') {
+    const url = `${urlStore.apiUrl}/posts?communityId=${props.communityId}?sort=${props.sort}&next=${next.value}`
+    const response = await userStore.makeRequest(url, 'GET')
+    const data: Posts = await response.json() || {posts: [], next: null}
+
+    posts.value.push(data)
+    next.value = data.next
+  } else {
+    const url = `${urlStore.apiUrl}/posts?sort=${props.sort}&next=${next.value}`
+    const response = await userStore.makeRequest(url, 'GET')
+    const data: Posts = await response.json() || {posts: [], next: null}
+
+    for (const post of data.posts) posts.value.push(post)
+    next.value = data.next
+  }
+
+  isFetching.value = false
+
+}
+
+function isBottom() {
+  return window.innerHeight + window.scrollY >= document.body.offsetHeight - 1080
+}
+
+onMounted(() => {
+  fetchPosts()
+
+  window.addEventListener('scroll', () => {
+    if (isBottom()) fetchPosts()
+  })
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', () => {
+    if (isBottom()) fetchPosts()
+  })
+});
+
+</script>
+
+<template>
+
+  <div class="feed" v-if="props.type === 'home' || props.type === 'community' && posts.length > 0">
+    <PostComponent v-for="post in posts" :key="post.publicId" :publicId="post.publicId" />
+  </div>
+
+  <div class="feed" v-else-if="props.type === 'user' && userFeed.length > 0">
+    <FeedItem v-for="feed in userFeed" :key="feed.item.publicId" :item="feed" />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/feed/FeedItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {defineProps, type PropType} from 'vue'
+import type { FeedItem } from '@/types/discuitTypes'
+
+import PostComponent from "@/components/post/PostComponent.vue";
+import EmbeddedComment from "@/components/comments/EmbeddedComment.vue";
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<FeedItem>,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <PostComponent v-if="item.type === 'post'" :public-id="item.item.publicId" />
+    <EmbeddedComment v-else-if="item.type === 'comment'" :comment="item.item" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
