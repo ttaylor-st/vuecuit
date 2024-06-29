419d9cf1e651c0677c1906953e46f0b2b8b9a0bf
Taylor <ttaylor-st@murena.io>
2024-06-29 03:39:45 +0100

    feat: add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	appId: "io.github.vuecuit",
+	appName: "vuecuit",
+	webDir: "dist",
+};
+
+export default config;
