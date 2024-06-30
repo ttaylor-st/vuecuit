ad74bd9c0a711c0e14a03d7c932185143754db15
Taylor <ttaylor-st@murena.io>
2024-06-30 17:56:09 +0100

    feat: support replies to comments

--- src/components/comments/EmbeddedComment.vue
+++ src/components/comments/EmbeddedComment.vue
@@ -17,6 +17,10 @@ const props = defineProps({
     type: Object as () => Comment,
     required: true,
   },
+  op: {
+    type: String,
+    required: false,
+  }
 });
 
 const timeElapsedHuman = timeSince(props.comment.createdAt);
@@ -30,15 +34,19 @@ const body = computed(() => {
 
 <template>
 
-  <div class="embedded-comment">
+  <div class="embedded-comment" :class="{ 'is-op': comment.author?.id === op }" :style="{ paddingBottom: comment.children?.length ? '0' : '0.5rem' }">
     <div class="comment-header">
       <div class="comment-user">
-        <img class="comment-user-icon" :src="comment.author.proPic?.url" alt="User profile picture">
-        <span class="comment-user-name">{{ comment.author.username }}</span>
+        <img class="comment-user-icon" :src="comment.author?.proPic?.url" />
+        <span class="comment-user-name">{{ comment.author?.username || "Hidden" }}</span>
       </div>
       <span class="comment-date" :title="humanReadableDate">{{ timeElapsedHuman }}</span>
     </div>
     <div class="comment-body" v-html="body"></div>
+
+    <div class="comment-replies" v-if="comment.children?.length">
+      <EmbeddedComment v-for="reply in comment.children" :key="reply.id" :comment="reply" :op="op" />
+    </div>
   </div>
 
 </template>
@@ -50,17 +58,42 @@ const body = computed(() => {
   flex-direction: column;
   gap: 0.5rem;
 
-  background-color: hsl(var(--primary-200) / 0.25);
-  border: 1px solid transparent;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
+  position: relative;
+  background-color: hsl(var(--primary-100) / 1);
+  padding: 0.5rem 0 0.5rem 0.5rem;
+  border-radius: 0 0.5rem 0.5rem 0;
   transition: all 0.3s ease;
+
+  &.is-op {
+    .comment-user-name {
+      color: hsla(var(--accent-900) / 1);
+    }
+
+    &::before {
+      background-color: hsla(var(--accent-900) / 1);
+    }
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0.3rem;
+    height: 100%;
+    border-radius: 0.5rem;
+    background-color: hsl(var(--primary-200) / 1);
+  }
+
+
+
 }
 
 .comment-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding-right: 0.5rem;
 }
 
 .comment-user {
@@ -73,10 +106,12 @@ const body = computed(() => {
   height: 30px;
   border-radius: 50%;
   margin-right: 0.5rem;
+  background-color: hsl(var(--primary-200) / 1);
 }
 
 .comment-user-name {
   font-weight: bold;
+  color: hsla(var(--primary-900) / 1);
 }
 
 .comment-date {
@@ -87,6 +122,11 @@ const body = computed(() => {
   margin-top: 0.5rem;
 }
 
+.comment-replies {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
 
 
 </style>
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -5,30 +5,49 @@ import { useUrlStore } from '@/stores/urlStore'
 import { ref, onMounted } from 'vue'
 import PostComponent from "@/components/post/PostComponent.vue";
 import EmbeddedComment from "@/components/comments/EmbeddedComment.vue";
+import type {Post} from "@/types/discuitTypes";
 
 const userStore = useUserStore()
 const urlStore = useUrlStore()
 
 const publicId = ref(useRoute().params.publicId)
+const post = ref<Post>()
 const comments = ref([])
+const commentCount = ref(0)
 
-// TODO: Support nested comments
+function buildComments(comments, parentId = null) {
+  return comments
+    .filter(comment => comment.parentId === parentId)
+    .map(comment => ({
+      ...comment,
+      children: buildComments(comments, comment.id)
+    }));
+}
+
 onMounted(async () => {
-  const fetched = await userStore
+  const fetchedComments = await userStore
       .makeRequest(`${urlStore.apiUrl}/posts/${publicId.value}/comments`, 'GET')
       .then((res) => res.data)
       .catch((err) => console.error(err))
 
-  comments.value = fetched.comments;
+  const fetchedPost = await userStore
+      .makeRequest(`${urlStore.apiUrl}/posts/${publicId.value}`, 'GET')
+      .then((res) => res.data)
+      .catch((err) => console.error(err))
+
+  comments.value = buildComments(fetchedComments.comments);
+  commentCount.value = fetchedComments.comments.length;
+  post.value = fetchedPost;
 })
 </script>
 
 <template>
 
-  <div>
+  <div style="padding: 1rem;">
     <PostComponent :publicId="publicId" :post="post" :comments="comments" />
     <section class="comments">
-      <EmbeddedComment v-for="comment in comments" :key="comment.id" :comment="comment" />
+      <h2>{{ commentCount }} Comments</h2>
+      <EmbeddedComment v-for="comment in comments" :key="comment.id" :comment="comment" :op="post.author.id" />
     </section>
   </div>
 
@@ -39,5 +58,6 @@ onMounted(async () => {
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  margin-top: 1rem;
 }
 </style>
