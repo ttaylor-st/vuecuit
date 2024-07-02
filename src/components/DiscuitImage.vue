58d23ce200a14b7afc2e608d845b04cff16bcc01
Taylor <ttaylor-st@murena.io>
2024-07-02 23:45:27 +0100

    feat: add lightbox to DiscuitImage

--- src/components/DiscuitImage.vue
+++ src/components/DiscuitImage.vue
@@ -2,6 +2,7 @@
 import { defineProps, ref } from 'vue'
 
 const imageLoaded = ref(false)
+const lightboxOpen = ref(false)
 
 const props = defineProps<{
   src: string,
@@ -11,31 +12,39 @@ const props = defineProps<{
 
 let loading = props.loading || 'lazy'
 if (loading !== 'lazy' && loading !== 'eager') loading = 'lazy'
+
+const toggleLightbox = () => {
+  lightboxOpen.value = !lightboxOpen.value
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') lightboxOpen.value = false
+})
+
+
 </script>
 
 <template>
-
   <div class="discuit-image">
     <img
       :src="props.src"
       :alt="props.alt"
       :loading="loading"
       @load="imageLoaded = true"
+      @click="toggleLightbox"
     />
-    <div
-      v-if="!imageLoaded"
-      class="skeleton"
-    ></div>
+    <div v-if="!imageLoaded" class="skeleton"></div>
+    <div v-if="lightboxOpen" class="lightbox" @click="toggleLightbox" @keydown.enter="toggleLightbox" role="button">
+      <img :src="props.src" :alt="props.alt" class="lightbox-img"/>
+    </div>
   </div>
-
 </template>
 
 <style scoped>
-
 .discuit-image {
   position: relative;
   width: 100%;
-  height: 200px;
+  max-height: 768px;
 }
 
 .discuit-image .skeleton {
@@ -47,4 +56,24 @@ if (loading !== 'lazy' && loading !== 'eager') loading = 'lazy'
   background-color: #f0f0f0;
 }
 
+.lightbox {
+  opacity: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(5px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  transition: opacity 0.3s;
+}
+
+.lightbox-img {
+  max-width: 90%;
+  max-height: 90%;
+}
 </style>
