6647a822efcde4afd479dfc76bcff367812cd940
Taylor <ttaylor-st@murena.io>
2024-07-12 22:21:57 +0100

    feat: change carousel item styles

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
-import 'vue-material-design-icons/styles.css'
-import './assets/css/main.scss'
-import { useUrlStore } from '@/stores/urlStore'
-import { useUserStore } from '@/stores/userStore'
-import { useSettingsStore } from '@/stores/settingsStore'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { App as CapacitorApp } from '@capacitor/app'
+import "vue-material-design-icons/styles.css";
+import "./assets/css/main.scss";
+import { useUrlStore } from "@/stores/urlStore";
+import { useUserStore } from "@/stores/userStore";
+import { useSettingsStore } from "@/stores/settingsStore";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { App as CapacitorApp } from "@capacitor/app";
 
-const USER_REFRESH_INTERVAL = 5000
+const USER_REFRESH_INTERVAL = 5000;
 
 function initializeApp(): ReturnType<typeof createApp> {
-  const app = createApp(App)
-  app.use(createPinia())
-  app.use(router)
-  app.provide('urlStore', useUrlStore())
-  app.provide('userStore', useUserStore())
-  app.provide('settingsStore', useSettingsStore())
-  return app
+  const app = createApp(App);
+  app.use(createPinia());
+  app.use(router);
+  app.provide("urlStore", useUrlStore());
+  app.provide("userStore", useUserStore());
+  app.provide("settingsStore", useSettingsStore());
+  return app;
 }
 
 function initializeTheme() {
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const isFirstTime = settingsStore.isFirstTime;
 
   if (isFirstTime) {
@@ -34,39 +34,41 @@ function initializeTheme() {
   const theme = settingsStore.getTheme;
 
   if (appElement) {
-    appElement.classList.add(isDarkMode ? 'dark' : 'light');
+    appElement.classList.add(isDarkMode ? "dark" : "light");
     appElement.classList.add(`${theme}`);
   }
 }
 
 async function initializeStores(): Promise<void> {
-  const localStorageToken = localStorage.getItem('csrfToken')
-  if (localStorageToken) await useUserStore().setXcsrfToken(localStorageToken)
-  useUrlStore().loadUrls()
-  await useUserStore().initial()
-  await useUserStore().fetchUser()
+  const localStorageToken = localStorage.getItem("csrfToken");
+  if (localStorageToken) await useUserStore().setXcsrfToken(localStorageToken);
+  useUrlStore().loadUrls();
+  await useUserStore().initial();
+  await useUserStore().fetchUser();
 }
 
 function startUserRefresh(): void {
   setInterval(async () => {
-    await useUserStore().fetchUser()
-  }, USER_REFRESH_INTERVAL)
+    await useUserStore().fetchUser();
+  }, USER_REFRESH_INTERVAL);
 }
 
-void CapacitorApp.addListener('backButton', ({canGoBack}) => {
+void CapacitorApp.addListener("backButton", ({ canGoBack }) => {
   if (!canGoBack) void CapacitorApp.exitApp();
   else window.history.back();
 });
 
 const app = initializeApp();
 const settingsStore = useSettingsStore();
-const appElement = document.getElementById('app');
+const appElement = document.getElementById("app");
 
 initializeStores();
 startUserRefresh();
 initializeTheme();
-app.mount('#app');
+app.mount("#app");
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-  settingsStore.toggleDarkMode();
-});
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
+    settingsStore.toggleDarkMode();
+  });
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -14,7 +14,10 @@ const defaultUrl = urlStore.defaultUrl
 
 const isDarkMode = ref(settingsStore.isDarkMode)
 const theme = ref(settingsStore.getTheme)
-const themes = [{ name: 'Default', value: 'default' }, { name: 'OLED', value: 'oled' }]
+const themes = [
+  { name: 'Default', value: 'default' },
+  { name: 'OLED', value: 'oled' }
+]
 
 const saveUrl = async (e: Event) => {
   e.preventDefault()
@@ -63,84 +66,120 @@ const toggleDarkMode = async () => {
   settingsStore.toggleDarkMode()
   isDarkMode.value = settingsStore.isDarkMode
 }
-
 </script>
 
 <template>
-
   <main>
-
     <h1>Settings</h1>
-    <p>Configure Vuecuit to match your needs. </p>
+    <p>Configure Vuecuit to match your needs.</p>
 
     <section class="theme">
       <h2>Theme</h2>
       <p>Change the appearance of Vuecuit.</p>
 
       <div class="tabs tabs-inline">
-        <button :class="{ active: isDarkMode }" @click="toggleDarkMode">Dark</button>
-        <button :class="{ active: !isDarkMode }" @click="toggleDarkMode">Light</button>
+        <button :class="{ active: isDarkMode }" @click="toggleDarkMode">
+          Dark
+        </button>
+        <button :class="{ active: !isDarkMode }" @click="toggleDarkMode">
+          Light
+        </button>
       </div>
 
       <div class="theme-carousel" v-if="isDarkMode">
-        <div v-for="thisTheme in themes" :key="thisTheme.value"
-          :class="{ 'theme-carousel__item': true, active: thisTheme.value + `-theme` === theme }"
-          @click="changeTheme(thisTheme.value)">
-          <img :src="`/src/assets/img/themes/dark-${thisTheme.value}.png`" :alt="`${thisTheme.name} Theme`" />
+        <div
+          v-for="thisTheme in themes"
+          :key="thisTheme.value"
+          :class="{
+            'theme-carousel__item': true,
+            active: thisTheme.value + `-theme` === theme,
+          }"
+          @click="changeTheme(thisTheme.value)"
+        >
+          <img
+            :src="`/src/assets/img/themes/dark-${thisTheme.value}.png`"
+            :alt="`${thisTheme.name} Theme`"
+          />
           <p>{{ thisTheme.name }}</p>
         </div>
       </div>
 
       <div class="theme-carousel" v-else>
-        <div v-for="thisTheme in themes" :key="thisTheme.value"
-          :class="{ 'theme-carousel__item': true, active: thisTheme.value + `-theme` === theme }"
-          @click="changeTheme(thisTheme.value)">
-          <img :src="`/src/assets/img/themes/light-${thisTheme.value}.png`" :alt="`${thisTheme.name} Theme`" />
+        <div
+          v-for="thisTheme in themes"
+          :key="thisTheme.value"
+          :class="{
+            'theme-carousel__item': true,
+            active: thisTheme.value + `-theme` === theme,
+          }"
+          @click="changeTheme(thisTheme.value)"
+        >
+          <img
+            :src="`/src/assets/img/themes/light-${thisTheme.value}.png`"
+            :alt="`${thisTheme.name} Theme`"
+          />
           <p>{{ thisTheme.name }}</p>
         </div>
       </div>
     </section>
-
 
     <section class="advanced">
       <h2>API Settings</h2>
       <p>Configure the API settings for Vuecuit.</p>
 
       <div class="warning">
-        <p>We recommend that you use the default settings unless you know what you are doing.</p>
+        <p>
+          We recommend that you use the default settings unless you know what
+          you are doing.
+        </p>
       </div>
 
       <form>
         <div class="form-group">
           <label for="base-url">Base URL</label>
-          <input type="text" id="api-url" name="base-url"
-            placeholder="https://api.example.com" v-model="baseUrl" />
+          <input
+            type="text"
+            id="api-url"
+            name="base-url"
+            placeholder="https://api.example.com"
+            v-model="baseUrl"
+          />
         </div>
-        
+
         <div class="form-actions">
-          <button class="secondary" @click="baseUrl = defaultUrl"
-                  type="reset">Reset</button>
-          <button @click="saveUrl"
-              type="submit">Save</button>
+          <button class="secondary" @click="baseUrl = defaultUrl" type="reset">
+            Reset
+          </button>
+          <button @click="saveUrl" type="submit">Save</button>
         </div>
       </form>
-
     </section>
 
     <section class="advanced">
       <h2>Cookies</h2>
-      <p>This section allows you to configure the cookies used by Vuecuit. In case signing in does not work, you can
-        manually set the CSRF token and session ID.</p>
+      <p>
+        This section allows you to configure the cookies used by Vuecuit. In
+        case signing in does not work, you can manually set the CSRF token and
+        session ID.
+      </p>
 
       <div class="warning">
-        <p>We recommend that you use the default settings unless you know what you are doing.</p>
+        <p>
+          We recommend that you use the default settings unless you know what
+          you are doing.
+        </p>
       </div>
 
       <form>
         <div class="form-group">
           <label for="csrftoken">CSRF Token</label>
-          <input type="text" id="csrftoken" name="csrftoken"
-            placeholder="csrf_token" v-model="csrfToken" />
+          <input
+            type="text"
+            id="csrftoken"
+            name="csrftoken"
+            placeholder="csrf_token"
+            v-model="csrfToken"
+          />
         </div>
 
         <div class="form-actions">
@@ -148,55 +187,65 @@ const toggleDarkMode = async () => {
           <button type="submit" @click="saveCsrfToken">Save</button>
         </div>
       </form>
-
     </section>
-
   </main>
-
 </template>
 
 <style scoped>
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+}
 
-  button {
-    padding: 0.5rem 1rem;
-    font-size: 1rem;
-    border-radius: 0.25rem;
-    cursor: pointer;
-  }
+.theme-carousel {
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+  overflow-x: auto;
 
-  .theme-carousel {
+  .theme-carousel__item {
+    position: relative;
     display: flex;
-    gap: 1rem;
-    margin-top: 1rem;
-    overflow-x: auto;
+    flex-direction: column;
+    overflow: hidden;
+    gap: 0.5rem;
 
-    .theme-carousel__item {
-      display: flex;
-      flex-direction: column;
-      gap: 0.5rem;
+    border-radius: 1rem;
+    border: 2px solid hsla(var(--background-200) / 0.5);
+    background-color: hsla(var(--background-200) / 0.5);
 
-      border-radius: 1rem;
-      padding: 0.5rem;
-      border: 2px solid hsla(var(--background-200) / 0.5);
-      background-color: hsla(var(--background-200) / 0.5);
-
-      &.active {
-        scale: 1;
-        background-color: hsla(var(--background-200) / 1);
-        border: 2px solid hsla(var(--accent-200) / 1);
-      }
-
-      img {
-        max-height: 512px;
-        border-radius: 1rem;
-      }
+    &.active {
+      background-color: hsla(var(--background-200) / 1);
+      border: 2px solid hsla(var(--accent-200) / 1);
 
       p {
-        text-align: center;
+        background-color: hsla(var(--accent-200) / 1);
+        opacity: 1;
+        bottom: 0;
       }
+    }
 
+    img {
+      max-height: 512px;
+    }
+
+    p {
+      position: absolute;
+      text-align: center;
+      width: 100%;
+      bottom: -3rem;
+      padding: 0.5rem;
+      opacity: 0;
+      transition: 0.25s;
+    }
+
+    &:hover p {
+      opacity: 1;
+      bottom: 0;
+      background-color: hsla(var(--background-200) / 1);
     }
   }
-
-
+}
 </style>
